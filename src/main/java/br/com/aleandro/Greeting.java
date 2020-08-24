5449127d89da1f22ffbae41232f2d0c81581fe72
Aleandro P. Dalan <aleandro.dalan@loglabdigital.com.br>
2020-08-24 16:01:17 -0400

    Hello SpringBoot and REST Udemy

---
+++ src/main/java/br/com/aleandro/Greeting.java
@@ -0,0 +1,12 @@
+package br.com.aleandro;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Greeting {
+
+	private final long id;
+	private final String content;
+}
